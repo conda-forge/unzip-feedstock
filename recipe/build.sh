a6811d6a80c5d9a8c8879f7bca46b5f390d6d972
Chris Burr <christopher.burr@cern.ch>
2022-02-02 14:10:31 +0100

    Update recipe based on Debian

---
+++ recipe/build.sh
@@ -0,0 +1,32 @@
+DEFINES="-DACORN_FTYPE_NFS"
+DEFINES="${DEFINES} -DWILD_STOP_AT_DIR"
+DEFINES="${DEFINES} -DLARGE_FILE_SUPPORT"
+DEFINES="${DEFINES} -DUNICODE_SUPPORT"
+DEFINES="${DEFINES} -DUNICODE_WCHAR"
+DEFINES="${DEFINES} -DUTF8_MAYBE_NATIVE"
+DEFINES="${DEFINES} -DDATE_FORMAT=DF_YMD"
+DEFINES="${DEFINES} -DIZ_HAVE_UXUIDGID"
+DEFINES="${DEFINES} -DNOMEMCPY"
+DEFINES="${DEFINES} -DNO_WORKING_ISPRINT"
+
+# Can't build with bzip2 support due to:
+# > A bzip2 library built with BZ_NO_STDIO should have an
+# > unresolved external, "bz_internal_error".  The default,
+# > full-function library will not mention it.
+# DEFINES="${DEFINES} -DUSE_BZIP2"
+
+
+if [[ ${target_platform} = linux-* ]]; then
+    DEFINES="${DEFINES} -DNO_LCHMOD"
+    make_target="unzips"
+else
+    make_target="generic"
+fi
+
+make -f unix/Makefile CC="$CC" prefix="$PREFIX" LFLAGS1="$LDFLAGS" CF="$CFLAGS -I. $DEFINES" "${make_target}"
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+    make prefix="$PREFIX" -f unix/Makefile check
+fi
+
+make prefix="$PREFIX" -f unix/Makefile install
